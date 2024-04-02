e45bbab11e280fba5fa60fd72e0ab37bebe33ab3
Bea.Anderson <Bea.Anderson@fdmgroup.com>
2024-04-02 17:52:21 +0100

    first cmit fixed

---
+++ src/main/java/com/beacodeart/Main.java
@@ -0,0 +1,7 @@
+package com.beacodeart;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/beacodeart/MergeSort.java
@@ -0,0 +1,12 @@
+package com.beacodeart;
+
+public class MergeSort {
+
+    private void merge() {
+
+    }
+
+    public static void mergeSort() {
+
+    }
+}
