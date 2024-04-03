75ff2a4f629e71999898c1c3ec47591cefc58c09
bea.anderson <bea.anderson@fdmgroup.com>
2024-04-03 15:47:26 +0100

    merge sort

--- src/main/java/com/beacodeart/Main.java
+++ src/main/java/com/beacodeart/Main.java
@@ -2,6 +2,12 @@ package com.beacodeart;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr1 = {1, 9, 2, 88, 5, 43};
+
+        MergeSort.mergeSort(arr1, 0, arr1.length-1);
+
+        for(int i: arr1){
+            System.out.println(i);
+        }
     }
 }
--- src/main/java/com/beacodeart/MergeSort.java
+++ src/main/java/com/beacodeart/MergeSort.java
@@ -2,11 +2,65 @@ package com.beacodeart;
 
 public class MergeSort {
 
-    private void merge() {
+    private static void merge(int[] arr, int start, int mid, int end) {
+        int sizeleft = (mid-start)+1;
+        int sizeright = (end-mid);
+
+        int[] leftArr = new int[sizeleft];
+        int[] rightArr = new int[sizeright];
+
+        int iterator = 0;
+
+        while (iterator<sizeleft) {
+            leftArr[iterator] = arr[start+iterator];
+            iterator++;
+        }
+
+        while (iterator<(end-start+1)){
+            rightArr[iterator-sizeleft] = arr[start+iterator];
+            iterator++;
+        }
+
+        int rightpointer = 0;
+        int leftpointer = 0;
+        int arrpointer = start;
+
+        while (rightpointer<rightArr.length && leftpointer<leftArr.length) {
+            if (rightArr[rightpointer]<leftArr[leftpointer]){
+                arr[arrpointer] = rightArr[rightpointer];
+                arrpointer++;
+                rightpointer++;
+            } else {
+                arr[arrpointer] = leftArr[leftpointer];
+                arrpointer++;
+                leftpointer++;
+            }
+        }
+
+        while (rightpointer<rightArr.length) {
+            arr[arrpointer] = rightArr[rightpointer];
+            arrpointer++;
+            rightpointer++;
+        }
+        while (leftpointer<leftArr.length) {
+            arr[arrpointer] = leftArr[leftpointer];
+            arrpointer++;
+            leftpointer++;
+        }
 
     }
 
-    public static void mergeSort() {
+    public static void mergeSort(int[] arr, int start, int end) {
+        if (end<=start){
+            return;
+        }
 
+        int midpoint = (start+end)/2;
+
+        mergeSort(arr, start, midpoint);
+        
+        mergeSort(arr, midpoint+1, end);
+        
+        merge(arr, start, midpoint, end);
     }
 }
