99d90594f2a78833bd23fef29fbcaeee08750d7c
Bea.Anderson <Bea.Anderson@fdmgroup.com>
2024-04-09 19:22:23 +0100

    dfs implemented

--- src/main/java/com/beacodeart/DFS.java
+++ src/main/java/com/beacodeart/DFS.java
@@ -2,4 +2,28 @@ package com.beacodeart;
 
 public class DFS {
     
+    public static boolean depthFirstSearch(TreeNode node, int value){
+        
+        if (node.value==value){
+            return true;
+        }
+        
+        if (node.left == null && node.right == null){
+            return false;
+        }
+        
+        if (node.left != null){
+            if (node.right != null){
+                return depthFirstSearch(node.left, value) || depthFirstSearch(node.right, value);
+            } else return depthFirstSearch(node.left, value);
+            
+        }
+        
+        if (node.right!=null){
+            return depthFirstSearch(node.right, value);
+        }
+        
+        return false;
+    }
+
 }
--- src/main/java/com/beacodeart/Main.java
+++ src/main/java/com/beacodeart/Main.java
@@ -2,12 +2,19 @@ package com.beacodeart;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = {1, 9, 2, 88, 5, 43};
+        int[] arr1 = { 1, 9, 2, 88, 5, 43 };
 
-        MergeSort.mergeSort(arr1, 0, arr1.length-1);
+        MergeSort.mergeSort(arr1, 0, arr1.length - 1);
 
-        for(int i: arr1){
+        for (int i : arr1) {
             System.out.println(i);
         }
+
+        TreeNode node4 = new TreeNode(98);
+        TreeNode node2 = new TreeNode(5, node4, null);
+        TreeNode node3 = new TreeNode(4);
+        TreeNode node1 = new TreeNode(1, node2, node3);
+
+        System.out.println(DFS.depthFirstSearch(node1, 98));
     }
 }
