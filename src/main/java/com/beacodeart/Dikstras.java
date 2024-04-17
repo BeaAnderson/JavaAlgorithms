29bc8ac329ef91c11bb4f8922c5a80d87ffe1cb2
bea.anderson <bea.anderson@fdmgroup.com>
2024-04-17 16:37:05 +0100

    formatting changes in dijkstras shortest path

--- src/main/java/com/beacodeart/Dikstras.java
+++ src/main/java/com/beacodeart/Dikstras.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Dikstras {
+    
     public static List<GraphNode> DSP(DijkstrasGraph graph, int source){
         
         NodeWrapper wrapper = graph.alist.get(source).get(0);
@@ -14,10 +15,13 @@ public class Dikstras {
         
 
         List<GraphNode> nodesWeCanGetTo = new ArrayList<>();
+        
         nodesWeCanGetTo.add(node);
 
         while (!nodesWeCanGetTo.isEmpty()) {
+            
             GraphNode minNode = null;
+            
             for (GraphNode currnode: nodesWeCanGetTo){
                 if(minNode==null){
                     minNode=currnode;
@@ -25,28 +29,35 @@ public class Dikstras {
                     minNode=currnode;
                 }
             }
+            
             knownShortestPath.add(minNode);
             minNode.known=true;
             nodesWeCanGetTo.remove(minNode);
+            
             List<NodeWrapper> nodesInAdjListOfMin = relax(graph.alist.get(minNode.data), minNode);
+
             for (NodeWrapper currNode: nodesInAdjListOfMin){
                 if(currNode.getNode().data!=minNode.data && !currNode.getNode().known && !nodesWeCanGetTo.contains(currNode.getNode())){
                     nodesWeCanGetTo.add(currNode.getNode());
                 }
             }
-        }
-
         
+        }
 
         return knownShortestPath;
     }
 
     private static List<NodeWrapper> relax(LinkedList<NodeWrapper> linkedList, GraphNode currentNode) {
+        
         for (NodeWrapper currWrapper: linkedList){
+            
             if(currWrapper.getNode().data != currentNode.data){
+                
                 if (currWrapper.getNode().dist>currentNode.dist+currWrapper.getEdge()){
+                    
                     currWrapper.getNode().dist=currentNode.dist+currWrapper.getEdge();
                     currWrapper.getNode().prev=currentNode;
+                
                 }
             }
         }
